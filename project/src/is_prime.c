3f78347432044d758e4edc80ba974af0bf6e92af
Кирилл <kiryuha_21@mail.ru>
2021-03-11 01:45:31 +0300

    fixed tabs??

--- project/src/is_prime.c
+++ project/src/is_prime.c
@@ -2,14 +2,14 @@
 
 int is_prime(int value) {
     if (value == 2) {
-		return 1;
-	} else if (value % 2 == 0 || value < 2) {
-		return 0;
-	}
+        return 1;
+    } else if (value % 2 == 0 || value < 2) {
+        return 0;
+    }
 
-	for (int i = 3; i * i < value; i+=2) {
-		if (value % i == 0) return 0;
-	}
+    for (int i = 3; i * i < value; i+=2) {
+        if (value % i == 0) return 0;
+    }
 
-	return 1;
+    return 1;
 }
