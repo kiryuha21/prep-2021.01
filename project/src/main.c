ecf3ac052920a106942ebe5241f8c4ca513e10af
Кирилл <kiryuha_21@mail.ru>
2021-03-11 01:02:04 +0300

    reupload for all files after branch remake

--- project/src/main.c
+++ project/src/main.c
@@ -1,66 +1,62 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "is_prime.h"
 #include "utils.h"
 
-#define ERR_ARGS_COUNT (-1)
-#define ERR_WRONG_FLG (-2)
+#define ERR_ARGS_COUNT -1
+#define ERR_WRONG_FLG -2
 
-#define TST_FOO_FIX     1
-#define TST_FOO_IMPL    2
-#define TST_MOD_IMPL    3
+#define TST_FOO_FIX 1
+#define TST_FOO_IMPL 2
+#define TST_MOD_IMPL 3
 
-
-/* NOTE(stitaevskiy):
- * We use `atoi` function just for simplification and code reducing.
- * This function doesn't report conversation errors.
- * For safety program we recommend using `strtol` and its analogs.
- * (See `man atoi` and `man strtol` for more info).
- *
- * const char str_num[] = "1234";
- * char* end = NULL;
- * int val = (int) strtol(str_num, &end, 0);
- * if (end != '\0') {
- *     //ERROR
- * }
- *
- * */
+int strtol_check(const char* end) {
+	if (*end != '\0') {
+		printf("Ошибка ввода\n");
+		return 0;
+	}
+	return 1;
+}
 
 int main(int argc, const char** argv) {
     if (argc < 3) {
         return ERR_ARGS_COUNT;
     }
 
-    int test_case = atoi(argv[1]);
-    const char* data;
-    data = argv[2];
+    char *end = NULL;
+    int test_case = strtol(argv[1], &end, 0);
+    if (!strtol_check(end)) {
+    	return ERR_WRONG_FLG;
+    }
+    const char* data = argv[2];
 
     switch (test_case) {
         case TST_FOO_FIX: {
-            size_t res = custom_strlen(data);
-            printf("%zu\n", res);
+            printf("%zu\n", custom_strlen(data));
             break;
         }
         case TST_FOO_IMPL: {
             if (argc != 4) {
                 return ERR_ARGS_COUNT;
             }
-            /* Comment to prevent `unused parameter` error
-            int base = atoi(data);
-            int pow =  atoi(argv[3]);
-            int res = custom_pow(base, pow);    // TODO: Implement me
-
-            printf("%i\n", res);
-            */
+            int base = strtol(data, &end, 0);
+            if (!strtol_check(end)) {
+    			return ERR_WRONG_FLG;
+    		}
+            int pow =  strtol(argv[3], &end, 0);
+            if (!strtol_check(end)) {
+    			return ERR_WRONG_FLG;
+    		}
+            printf("%i\n", custom_pow(base, pow));
             break;
         }
         case TST_MOD_IMPL: {
-            // Comment for prevent `unused variable` error
-            // int num = atoi(data);
-
-            // TODO: Print to stdout `1` if `num` is prime number and `0` otherwise
-            // This function MUST be implemented in
-            // a separate C-module (not in `main` or `utils` module)
+            int num = strtol(data, &end, 0);
+            if (!strtol_check(end)) {
+    			return ERR_WRONG_FLG;
+    		}
+            printf("%d\n", is_prime(num));
             break;
         }
         default: {
--- project/src/utils.c
+++ project/src/utils.c
@@ -3,12 +3,23 @@
 #include "utils.h"
 
 size_t custom_strlen(const char* str) {
-    return sizeof(str);
+    return strlen(str);
 }
 
-// TODO: Implement `power of` function
-/*
 int custom_pow(int base, int power) {
-    return 0;
+	if (base == 1 || power == 0) {
+		return 1;
+		} else {
+			int start = base;
+			for (int i = 0; i < power / 2 - 1; ++i) {
+				base *= start;
+			}
+
+			if (power % 2 == 0) {
+				base *= base;
+			} else {
+				base *= (base * start);
+			}
+    		return base;
+		}
 }
-*/
