5c5e46f7df177e87a8348d80a51a712b37f21f85
Кирилл <kiryuha_21@mail.ru>
2021-04-14 23:26:00 +0300

    excess files removed

--- project/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kiryuha21/prep-2021.01/project
-# Build directory: /home/kiryuha21/prep-2021.01/project
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
